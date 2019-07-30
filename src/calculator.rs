b5ee447de0ffb41136aff640c5cead3bfcfff51d
CK <nankeen@users.noreply.github.com>
2019-07-30 17:54:35 +0800

    Implemented single digit calculator

--- src/calculator.rs
+++ src/calculator.rs
@@ -1,8 +1,7 @@
-use std::mem::discriminant;
 use std::vec::Vec;
 
 // Runs with default radix 10
-const RADIX: u32 = 10;
+// const RADIX: u32 = 10;
 
 #[derive(Debug)]
 pub enum Token {
@@ -12,78 +11,143 @@ pub enum Token {
 }
 
 #[derive(Debug)]
-pub enum InterpreterFSM {
+pub enum State {
     Start,
     Integer,
-    Operator,
+    Add,
     End,
 }
 
-impl InterpreterFSM {
-    pub fn next(self, event: Token) -> InterpreterFSM {
-        match self {
-            InterpreterFSM::Start => handleStart(event);
-        }
-    }
-
-    fn handleStart(event: Token) -> InterpreterFSM {
-        match event {
-            Token::Integer(i) => InterpreterFSM::Integer,
-        }
-    }
-}
-
 pub struct Interpreter {
-    token_iterator: std::str::Chars<'static>,
-    current_token: Token,
-    stack: Vec<u32>,
+    pub state: State,
+    pub stack: Vec<u32>,
 }
 
 impl Interpreter {
-    pub fn new(program: &'static str) -> Interpreter {
+    pub fn new() -> Interpreter {
         Interpreter {
-            token_iterator: program.chars(),
-            current_token: Token::End,
+            state: State::Start,
             stack: Vec::new(),
         }
     }
 
-    fn eat(&mut self, token_type: &Token) {
-        let t = self.next_token();
-        if discriminant(&t) == discriminant(token_type) {
-            self.current_token = t;
-        } else {
-            panic!("Unexpected token type");
-        }
+    pub fn reset(&mut self) {
+        self.state = State::Start;
+        self.stack.clear();
     }
 
-    pub fn next_token(&mut self) -> Token {
-        let n = self.token_iterator.next();
-        match n {
-            Some(c) => match c {
-                c if c.is_digit(RADIX) => {
-                    Token::Integer(c.to_digit(RADIX).unwrap())
-                },
-                '+' => Token::Operator(c),
-                _ => panic!("Unknown token"),
+    pub fn next(&mut self, event: Token) -> State {
+        use State::*;
+        match self.state {
+            Start => self.handle_start(event),
+            Integer => self.handle_digit(event),
+            Add => self.handle_add(event),
+            End => {
+                match self.stack.pop() {
+                    Some(ret) => println!("{:?}", ret),
+                    None => println!("nil"),
+                };
+                State::End
             },
-            None => Token::End,
+            /*
+            Operator => self.handle_operator(event),
+            End => End,
+            */
         }
     }
 
-    pub fn eval(&mut self) -> u32 {
-        self.current_token = self.next_token();
-        loop {
-            match self.current_token {
-                Token::Integer(i) => {
-                    self.eat(&Token::Operator('+'));
-                    self.stack.push(i);
-                },
-                Token::Operator(_) => self.eat(&Token::Integer(0)),
-                Token::End => break,
-            }
+    fn handle_start(&mut self, event: Token) -> State {
+        use Token::*;
+        match event {
+            Integer(d) => {
+                self.stack.push(d);
+                State::Integer
+            },
+            End => State::End,
+            _ => panic!("Syntax error"),
         }
+    }
 
-        return self.stack.iter().sum();
+    fn handle_digit(&mut self, event: Token) -> State {
+        use Token::*;
+        match event {
+            Operator(op) => match op {
+                '+' => State::Add,
+                _ => panic!("Invalid operator"),
+            },
+            End => State::End,
+            _ => panic!("Syntax error"),
+        }
+    }
+
+    fn handle_add(&mut self, event: Token) -> State {
+        use Token::*;
+        match event {
+            Integer(d) => {
+                let a = self.stack.pop().unwrap();
+                self.stack.push(a+d);
+                State::Integer
+            },
+            _ => panic!("Syntax error")
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::mem::discriminant;
+    
+    macro_rules! assert_enum_eq {
+        ($a:expr,$b:expr) => {
+            assert_eq!(discriminant($a), discriminant($b));
+        };
+    }
+
+    #[test]
+    #[should_panic(expected = "Syntax error")]
+    fn test_start_state() {
+        // Start should transition to digit state
+        let mut interpreter = Interpreter::new();
+        let mut ns = interpreter.next(Token::Integer(1));
+        assert_enum_eq!(&ns, &State::Integer);
+
+        // Test invalid transition panic
+        interpreter.next(Token::Operator('+'));
+
+        // Test digit can end
+        ns = interpreter.next(Token::End);
+        assert_enum_eq!(&ns, &State::End);
+
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid operator")]
+    fn test_integer_state() {
+        // Integer should transition to `Add` state
+        let mut interpreter = Interpreter::new();
+        interpreter.state = interpreter.next(Token::Integer(1));
+
+        let mut ns = interpreter.next(Token::Operator('+'));
+        assert_enum_eq!(&ns, &State::Add);
+
+        // Test invalid operator panic
+        interpreter.next(Token::Operator('>'));
+
+        // Test digit can end
+        ns = interpreter.next(Token::End);
+        assert_enum_eq!(&ns, &State::End);
+    }
+
+    #[test]
+    fn test_add_state() {
+        // Add should transition to `Integer` state
+        let mut interpreter = Interpreter::new();
+        interpreter.state = interpreter.next(Token::Integer(5));
+        interpreter.state = interpreter.next(Token::Operator('+'));
+
+        let ns = interpreter.next(Token::Integer(3));
+        assert_enum_eq!(&ns, &State::Integer);
+        assert_eq!(interpreter.stack.pop().unwrap(), 5+3);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
+use std::io;
+use std::io::prelude::*;
+
 mod calculator;
 
 fn main() {
-    let mut interpreter = calculator::Interpreter::new("5+3");
-    println!("{:?}", interpreter.eval());
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let mut interpreter = calculator::Interpreter::new();
+    for line in stdin.lock().lines() {
+        interpreter.reset();
+        for e in line.unwrap().chars() {
+            let token = match e {
+                e if e.is_digit(10) => {
+                    calculator::Token::Integer(e.to_digit(10).unwrap())
+                },
+                '+' => calculator::Token::Operator('+'),
+                _ => panic!("Syntax error"),
+            };
+            interpreter.state = interpreter.next(token);
+        }
+        interpreter.next(calculator::Token::End);
+        println!("{:?}", interpreter.stack);
+    }
 }
